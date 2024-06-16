27e95b52398285924b9da5a06d612bf627d6e3f3
MarceloSantos1906 <marcelo_santos1906@hotmail.com>
2024-06-16 12:14:13 -0300

    v 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,44 @@
-import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet, Link } from 'react-router-dom';
 import Pages from './Pages';
 import './App.css';
 
 export default function App() {
   return (
-    <>
+    <Router>
       <div className="container">
         <h2>Exemplo Rotas</h2>
-        </div>
-        <div className="col-auto">
-          <Routes>
-            <Route path='/' element={<Layout/>}>
-              <Route index element={<Pages.Home/>}/>
-              <Route path='register' element={<Pages.Register/>}/>
-
-              <Route path='*' element={<Pages.NotFound/>}/>
-            </Route>
-          </Routes>
-        </div>
-    </>
+      </div>
+      <div className="col-auto">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Pages.Home />} />
+            <Route path="register" element={<Pages.Register />} />
+            <Route path="*" element={<Pages.NotFound />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 function Layout() {
-  return(
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/register'>Register</Link>
-        </li>
-        <li>
-          <Link to='/*'>Nothing Here</Link>
-        </li>
-      </ul>
-    </nav>
-    <hr/>
-    <Outlet/>
-  </div>
-  )
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/non-existent">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  );
 }
